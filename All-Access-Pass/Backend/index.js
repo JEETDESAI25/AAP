a887d88cab15bb01d3dbd71839d20ead0168616b
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-23 18:04:27 -0700

    Backend setup

---
+++ All-Access-Pass/Backend/index.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// In-memory data storage for demo purposes (Replace with a proper database in production)
+const users = [];
+
+// Register route
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = {
+      id: users.length + 1,
+      email,
+      password: hashedPassword,
+    };
+
+    users.push(newUser);
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Registration failed" });
+  }
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = users.find((u) => u.email === email);
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    // Create and sign a JWT token
+    const token = jwt.sign({ userId: user.id }, "your_jwt_secret", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
