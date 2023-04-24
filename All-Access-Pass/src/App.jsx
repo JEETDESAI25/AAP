98760444e3137887d1edaaf0739c0893bd56c02b
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-23 17:03:19 -0700

    Created LoginPage

--- All-Access-Pass/src/App.jsx
+++ All-Access-Pass/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import GalleryPage from "./pages/GalleryPage";
 import RewardsPage from "./pages/RewardsPage";
 import EventPage from "./pages/EventPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/rewards" element={<RewardsPage />} />
           <Route path="/events" element={<EventPage />} />
---
+++ All-Access-Pass/src/pages/LoginPage.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "../styles/LoginPage.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login action here
+    console.log("Email:", email, "Password:", password);
+  };
+
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
