03565133885ca4fec397e56627b3d63521334ba3
JEETDESAI25 <jeetdesai25@gmail.com>
2023-05-03 18:05:40 -0700

    Updated

--- All-Access-Pass/src/pages/EventPage.jsx
+++ All-Access-Pass/src/pages/EventPage.jsx
@@ -1,9 +1,49 @@
-import "../styles/EventPage.css";
+import "/Users/jd/Documents/Web Dev/AAP/All-Access-Pass/src/styles/EventPage.css";
+
+const events = [
+  {
+    title: "Event 1",
+    date: "2023-06-10",
+    description: "This is a description of Event 1.",
+    image:
+      "https://imgresizer.eurosport.com/unsafe/2560x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2014/05/25/1243778-26866391-2560-1440.jpg",
+  },
+  {
+    title: "Event 2",
+    date: "2023-07-15",
+    description: "This is a description of Event 2.",
+    image:
+      "https://c8.alamy.com/comp/2GHA04W/real-madrid-fans-during-the-uefa-champions-league-semi-final-2nd-leg-match-at-the-santiago-bernabeu-stadium-photo-credit-should-read-philip-oldhamsportimage-via-pa-images-2GHA04W.jpg",
+  },
+  {
+    title: "Event 3",
+    date: "2023-08-05",
+    description: "This is a description of Event 3.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI64PL89mAx2q9TwbNa4-CynbrkPmG3mN6BQ&usqp=CAU",
+  },
+];
 
 const EventPage = () => {
   return (
-    <div>
-      <h1>Participate in Events</h1>
+    <div className="event-page">
+      <h2>Upcoming Events</h2>
+      <div className="events-container">
+        {events.map((event, index) => (
+          <div key={index} className="event-card">
+            <div className="event-image-container">
+              <img
+                src={event.image}
+                alt={event.title}
+                className="event-image"
+              />
+            </div>
+            <h3 className="event-title">{event.title}</h3>
+            <p className="event-date">{event.date}</p>
+            <p className="event-description">{event.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- All-Access-Pass/src/pages/RewardsPage.jsx
+++ All-Access-Pass/src/pages/RewardsPage.jsx
@@ -2,12 +2,12 @@ import "../styles/RewardsPage.css";
 
 const RewardPage = () => {
   const images = [
-    "https://example.com/image1.jpg",
-    "https://example.com/image2.jpg",
-    "https://example.com/image3.jpg",
-    "https://example.com/image4.jpg",
-    "https://example.com/image5.jpg",
-    "https://example.com/image6.jpg",
+    "https://i.etsystatic.com/8759390/r/il/c2681b/2403354424/il_1588xN.2403354424_gkdf.jpg",
+    "https://i.etsystatic.com/29958485/r/il/c83a85/4777163463/il_1588xN.4777163463_sxpr.jpg",
+    "https://i.etsystatic.com/29958485/r/il/9c2cee/4921473047/il_1588xN.4921473047_9b2o.jpg",
+    "https://i.etsystatic.com/19684008/r/il/7201b8/4690284952/il_1588xN.4690284952_4ffy.jpg",
+    "https://i.etsystatic.com/42328365/r/il/f83b6b/4830239025/il_1588xN.4830239025_jk48.jpg",
+    "https://i.etsystatic.com/42328365/r/il/b76f95/4781967964/il_1588xN.4781967964_lyu7.jpg",
   ];
 
   return (
